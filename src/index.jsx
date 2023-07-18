1b57b8238a2763412d87a12203ff4ebbaa4752d8
David <doladepo128@gmail.com>
2023-07-18 23:12:26 +0100

    finished app setup and file structure

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { ContexProvider  } from './contexts/ContextProvider';
 import App from './App';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ContexProvider><App /></ContexProvider>, document.getElementById('root'));
